28f803a4d93f35a3ad59e569663e928219c27f58
Bao Nguyen <baonguyen@Baos-Mac-mini.local>
2017-02-15 17:33:43 +0700

    first add login form, add otocat image

---
+++ AwesomeProject/src/login/LoginForm.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class LoginForm extends Component {
+  render() {
+    return ();
+  }
+}
